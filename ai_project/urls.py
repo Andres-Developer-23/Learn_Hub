583755c40035d8e99e16004a3ee42f8824b2cf3a
Andres <andres01032001@gmail.com>
2026-05-19 12:29:16 -0500

    Agrega recuperación de contraseña con enlace por correo

--- ai_project/urls.py
+++ ai_project/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -26,6 +27,30 @@ urlpatterns = [
     path('estudiante/', include('student_portal.urls')),
     path('terminal/', include('terminal.urls')),
     path('notificaciones/', include('notificaciones.urls')),
+
+    # Password Reset
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='registration/password_reset_form.html',
+             email_template_name='registration/password_reset_email.html',
+             subject_template_name='registration/password_reset_subject.txt',
+         ),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='registration/password_reset_done.html',
+         ),
+         name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='registration/password_reset_confirm.html',
+         ),
+         name='password_reset_confirm'),
+    path('password-reset/complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='registration/password_reset_complete.html',
+         ),
+         name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
